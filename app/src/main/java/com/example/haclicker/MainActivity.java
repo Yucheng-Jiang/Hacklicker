f7e6414426801aee357c1a640625298b8eaff82b
Yucheng-Jiang <yj17@illinois.edu>
2020-08-07 10:13:43 -0400

    Add DashBoardScreen, adjust ConsoleScreen layout

--- app/src/main/java/com/example/haclicker/MainActivity.java
+++ app/src/main/java/com/example/haclicker/MainActivity.java
@@ -93,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             FirebaseUser user = mAuth.getCurrentUser();
-                            Intent intent = new Intent(getApplicationContext(), ConsoleScreen.class);
+                            Intent intent = new Intent(getApplicationContext(), DashBoardScreen.class);
                             startActivity(intent);
                         } else {
                             // If sign in fails, display a message to the user.
