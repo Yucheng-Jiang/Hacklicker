a2589e50c6ef93742b41321bca20db34b55dcffb
david-jiang <yueqij2@illinois.edu>
2020-08-07 13:57:51 -0400

    Modify data structure.

--- app/src/main/java/com/example/haclicker/DataStructure/ClassRoom.java
+++ app/src/main/java/com/example/haclicker/DataStructure/ClassRoom.java
@@ -1,15 +1,17 @@
-package com.example.haclicker;
+package com.example.haclicker.DataStructure;
 
 import java.util.List;
 
 public class ClassRoom {
     private String classID;
     private String className;
+    private String instructor;
     private List<Question> questions;
 
-    public ClassRoom(String classID, String className, List<Question> questions) {
+    public ClassRoom(String classID, String className, String instructor, List<Question> questions) {
         this.classID = classID;
         this.className = className;
+        this.instructor = instructor;
         this.questions = questions;
     }
 
@@ -20,6 +22,8 @@ public class ClassRoom {
     public String getClassName() {
         return className;
     }
+
+    public String getInstructor() { return instructor; }
 
     public List<Question> getQuestions() {
         return questions;
--- app/src/main/java/com/example/haclicker/MainActivity.java
+++ app/src/main/java/com/example/haclicker/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.haclicker.DataStructure.*;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -20,6 +21,11 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private GoogleSignInClient mGoogleSignInClient;
@@ -40,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //TODO: Delete send temp classroom to server.
+        sendTempData();
+        sendTempStuResponse();
         mAuth = FirebaseAuth.getInstance();
         createRequest();
         findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
@@ -102,4 +111,42 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
+    public void sendTempData() {
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference ref;
+        ref = db.getReference("ClassRooms");
+        List<String> choices = new ArrayList<String>() {
+            {add("A");
+            add("B");
+            add("C");
+            add("D");
+            add("E");
+            }
+        };
+        List<Question> questionList = new ArrayList<>();
+        questionList.add(new Question("fuck?", 0,
+                choices));
+        questionList.add(new Question("damn?", 01,
+                choices));
+        List<String> corans = new ArrayList<String>() {{add("A"); add("E");}};
+
+        questionList.get(0).setCorrectAns(corans);
+        ref.child("CS126").setValue(new ClassRoom("0", "CS126", "Michael Woodley", questionList));
+        System.out.println("fuck");
+    }
+
+    public void sendTempStuResponse() {
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference ref;
+        ref = db.getReference("ClassRooms").child("CS126").child("StudentResponse");
+        List<String> ans = new ArrayList<>();
+        ans.add("A");
+        ans.add("B");
+        StudentResponse studentResponse1 = new StudentResponse("Yucheng Jiang",
+                "ycj2@illinois.edu", ans, 0, System.currentTimeMillis());
+        StudentResponse studentResponse2 = new StudentResponse("Yueqi Jiang",
+                "yueqij2@illinois.edu", ans, 0, System.currentTimeMillis());
+        ref.child(studentResponse1.getQuestionID() + "").child(studentResponse1.getStudentName()).setValue(studentResponse1);
+        ref.child(studentResponse2.getQuestionID() + "").child(studentResponse2.getStudentName()).setValue(studentResponse2);
+    }
 }
