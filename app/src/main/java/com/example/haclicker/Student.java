064a4116fab666e80b87b153abf9d7bc37f2c891
david-jiang <yueqij2@illinois.edu>
2020-08-06 22:57:57 -0400

    Add teacher/student interaction struct.

---
+++ app/src/main/java/com/example/haclicker/Student.java
@@ -0,0 +1,12 @@
+package com.example.haclicker;
+
+public class Student {
+
+    public void sendResponse(StudentResponse studentResponse) {
+        //TODO: Send student response to server
+    }
+
+    public void updateStudentResponse(StudentResponse studentResponse) {
+        //TODO: Send updated response to server
+    }
+}
