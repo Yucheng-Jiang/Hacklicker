be23535682148f945889363a5fd99dd7a0e33c35
Yucheng-Jiang <yj17@illinois.edu>
2020-08-06 21:43:42 -0400

    Finish Google Authentication Login

---
+++ app/src/main/java/com/example/haclicker/ConsoleScreen.java
@@ -0,0 +1,45 @@
+package com.example.haclicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ConsoleScreen extends AppCompatActivity {
+
+    Button logout;
+    TextView name, mail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_console_screen);
+
+        logout = findViewById(R.id.logout);
+        name = findViewById(R.id.name);
+        mail = findViewById(R.id.mail);
+
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if (signInAccount != null) {
+            name.setText(signInAccount.getDisplayName());
+            mail.setText(signInAccount.getEmail());
+        }
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
