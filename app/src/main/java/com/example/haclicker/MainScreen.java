a63e69b8eff6d8581f1aae2c17ec9153f57ed6f0
david-jiang <yueqij2@illinois.edu>
2020-08-07 18:29:34 -0400

    Complete host side send data [Server interaction].

--- app/src/main/java/com/example/haclicker/MainScreen.java
+++ app/src/main/java/com/example/haclicker/MainScreen.java
@@ -5,15 +5,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.haclicker.DataStructure.ClassRoom;
+import com.example.haclicker.DataStructure.Teacher;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MainScreen extends AppCompatActivity {
 
     ImageButton settings, createRoom, joinRoom;
-    String username, userEmail;
+    String username, userEmail, className;
+
+    public static final int ID_LENGTH = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +55,50 @@ public class MainScreen extends AppCompatActivity {
         createRoom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO: generate classroom;
+                ClassRoom classroom = generateClassroom();
+                //TODO:upload to database
             }
         });
+    }
+
+    private ClassRoom generateClassroom() {
+
+        List<String> allRoomIDs = getAllRooms();
+        Random random = new Random();
+        while (true) {
+            StringBuilder id = new StringBuilder();
+            for (int i = 0; i < ID_LENGTH; i++) {
+                id.append(random.nextInt());
+            }
+            if (!allRoomIDs.contains(id.toString())) {
+                return new ClassRoom(id.toString(), id.toString(), username, null);
+            }
+        }
+    }
+
+    /**
+     * Check data base for all roomIDs to prevent duplicate ID.
+     * @return all roomIDs
+     */
+    private List<String> getAllRooms() {
+
+        final List<String> allRoomIDS = new ArrayList<>();
+        DatabaseReference reference = FirebaseDatabase.getInstance()
+                .getReference("ClassRooms");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ID : snapshot.getChildren()) {
+                    allRoomIDS.add(ID.child("classID").getValue().toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return allRoomIDS;
     }
 }
