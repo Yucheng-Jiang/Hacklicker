5c1b78251d6886e149e5be2413d8a7ef5ef19e12
david-jiang <yueqij2@illinois.edu>
2020-08-08 18:13:12 -0400

    Fix fatal exit after join room

--- app/src/main/java/com/example/haclicker/DataStructure/Student.java
+++ app/src/main/java/com/example/haclicker/DataStructure/Student.java
@@ -7,10 +7,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
--- app/src/main/java/com/example/haclicker/DataStructure/Teacher.java
+++ app/src/main/java/com/example/haclicker/DataStructure/Teacher.java
@@ -1,8 +1,24 @@
 package com.example.haclicker.DataStructure;
 
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,6 +28,8 @@ public class Teacher {
 
     private static ClassRoom classroom;
     private static List<Question> questionsToAdd;
+    //TODO:Add test path
+    private static String EXPORT_CSV_FILE_PATH = "";
 
 
     public static void setClassroom(ClassRoom setClassRoom) {
@@ -126,5 +144,87 @@ public class Teacher {
 
     public static List<Question> getQuestionsToAdd() {
         return questionsToAdd;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static void exportResultAsCSV() throws IOException {
+        List<StudentResponse> allStudentResponse = retrieveAllResponse();
+        try (
+            BufferedWriter writer = Files.newBufferedWriter(Paths.get(EXPORT_CSV_FILE_PATH));
+            CSVPrinter csvPrinter = new CSVPrinter(writer,
+                    CSVFormat.DEFAULT
+                    .withHeader("Name", "Email", "Answer", "QuestionID", "Time Stamp", "Correct"))
+        ) {
+            for (StudentResponse studentResponse : allStudentResponse) {
+                String name = studentResponse.getStudentName();
+                String email = studentResponse.getStudentEmail();
+                StringBuilder answerBuilder = new StringBuilder();
+                for (String ans : studentResponse.getAnswer()) {
+                    answerBuilder.append(ans);
+                }
+                String answer = answerBuilder.toString();
+                int questionID = studentResponse.getQuestionID();
+                long timeStamp = studentResponse.getTimeStamp();
+                boolean correctness = false;
+                StringBuilder correctAnswerBuilder = new StringBuilder();
+                for (String single : classroom.getQuestions().get(questionID).getCorrectAns()) {
+                    correctAnswerBuilder.append(single);
+                }
+                //check if student answer is correct
+                if (answer.equals(correctAnswerBuilder.toString()))
+                    correctness = true;
+                csvPrinter.printRecord(name, email, answer, questionID, timeStamp, correctness);
+            }
+        }
+        allStudentResponse.clear();
+    }
+
+    /**
+     * Retrieve all student response of a certain class.
+     * @return list of student response
+     */
+    private static List<StudentResponse> retrieveAllResponse() {
+
+        final List<StudentResponse> allStudentResponse = new ArrayList<>();
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        //~ClassRooms/classID/StudentResponse
+        DatabaseReference ref = db.getReference("ClassRooms")
+                .child(Teacher.getClassroom().getClassID())
+                .child("StudentResponse");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                //~ClassRooms/classID/StudentResponse/questionID
+                for (DataSnapshot singleQuestion : snapshot.getChildren()) {
+                    //~ClassRooms/classID/StudentResponse/questionID/StuName
+                    for (DataSnapshot singleResponse : singleQuestion.getChildren()) {
+                        List<String> answers = new ArrayList<>();
+                        //~ClassRooms/classID/StudentResponse/questionID/StuName/answer
+                        for (DataSnapshot ans : singleResponse.child("answer").getChildren()) {
+                            answers.add(ans.getValue().toString());
+                        }
+                        int questionID = Integer.parseInt(singleResponse
+                                .child("questionID").getValue().toString());
+                        String stuEmail = singleResponse.child("studentEmail").getValue().toString();
+                        String stuName = singleResponse.child("studentName").getValue().toString();
+                        long timeStamp = Long.parseLong(singleResponse
+                                .child("timeStamp").getValue().toString());
+                        allStudentResponse.add(new StudentResponse(stuName,
+                                stuEmail, answers, questionID, timeStamp));
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return allStudentResponse;
+    }
+
+    public void setExportCSVPath(String relativePath) {
+        EXPORT_CSV_FILE_PATH = relativePath;
     }
 }
