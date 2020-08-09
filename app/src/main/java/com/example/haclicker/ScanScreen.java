f24e52f9d0cff2a712a5d401a45cfbb420f79644
thenewpyjiang <pengyue3@illinois.edu>
2020-08-09 19:30:04 +0800

    Implement qr code scan

---
+++ app/src/main/java/com/example/haclicker/ScanScreen.java
@@ -0,0 +1,107 @@
+// Referenced from: https://www.youtube.com/watch?v=otkz5Cwdw38
+// Backup reference: https://www.youtube.com/watch?v=wfucGSKngq4
+package com.example.haclicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import com.google.zxing.Result;
+
+import static android.Manifest.permission.CAMERA;
+
+
+public class ScanScreen extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private static final int REQUEST_CAMERA = 1;
+    private ZXingScannerView scannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_screen);
+
+        scannerView = new ZXingScannerView(this);
+        setContentView(scannerView);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(ScanScreen.this, CAMERA)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(ScanScreen.this, "Permission is granted!", Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[] {CAMERA}, REQUEST_CAMERA);
+            }
+        }
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        String classID = result.getText();
+        // TODO: Do something with the scanned result
+    }
+
+    public void onRequestPermissionResult(int requestCode, String[] permission, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA:
+                if (grantResults.length > 0) {
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (cameraAccepted) {
+                        Toast.makeText(ScanScreen.this, "Permission Granted", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(ScanScreen.this, "Permission Denied", Toast.LENGTH_LONG).show();
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            if (shouldShowRequestPermissionRationale(CAMERA)) {
+                                displayAlertMessage("Permission required for scan", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        requestPermissions(new String[] {CAMERA}, REQUEST_CAMERA);
+                                    }
+                                });
+                            }
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(ScanScreen.this, CAMERA)
+                    == PackageManager.PERMISSION_GRANTED) {
+                if (scannerView == null) {
+                    scannerView = new ZXingScannerView(this);
+                    setContentView(scannerView);
+                }
+                scannerView.setResultHandler(this);
+                scannerView.startCamera();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[] {CAMERA}, REQUEST_CAMERA);
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        scannerView.stopCamera();
+    }
+
+    public void displayAlertMessage(String message, DialogInterface.OnClickListener listener) {
+        new AlertDialog.Builder(ScanScreen.this)
+                .setMessage(message)
+                .setPositiveButton("OK", listener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+}
