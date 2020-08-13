00015e6c062818235523049ee5eda4e34bb2cc26
Yucheng-Jiang <yj17@illinois.edu>
2020-08-12 23:32:15 -0400

    Finish history UI draft

---
+++ app/src/main/java/com/example/haclicker/HistoryDetailScreen.java
@@ -0,0 +1,57 @@
+package com.example.haclicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class HistoryDetailScreen extends AppCompatActivity {
+    private String role;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_detail_screen);
+
+        final TextView descriptionTxt = findViewById(R.id.descriptionTxt);
+        final Button copyBtn = findViewById(R.id.copyBtn);
+        role  = getIntent().getStringExtra("role");
+
+        descriptionTxt.setText(getIntent().getStringExtra("detail"));
+        copyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("", descriptionTxt.getText());
+                clipboard.setPrimaryClip(clip);
+
+                // Code below are cited from
+                // https://stackoverflow.com/questions/22194761/hide-textview-after-some-time-in-android
+                copyBtn.setText("TXT COPIED");
+                copyBtn.setTextColor(Color.GREEN);
+                copyBtn.postDelayed(new Runnable() {
+                    public void run() {
+                        copyBtn.setTextColor(Color.BLACK);
+                        copyBtn.setText("COPY TEXT");
+                    }
+                }, 1500);
+                // citation ends here
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(getApplication(), HistoryQuestionScreen.class);
+        intent.putExtra("role", role);
+        startActivity(intent);
+        finish();
+    }
+}
